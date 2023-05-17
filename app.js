3e6c53874fb8b41a4c70fd42bd46c1a9b4511177
bia004 <84ss611@gmail.com>
2023-05-17 14:48:52 -0400

    Add guess display

--- app.js
+++ app.js
@@ -1,6 +1,23 @@
 const guessBtn = document.querySelector('.guess-btn')
 const guessInput = document.querySelector('input[type=number]')
+const answers = document.querySelector('.number')
 
+const getRandNum = ()=> {
+    return Math.floor(Math.random()*100) + 1
+}
+
+guessInput.addEventListener("change", ()=>{
+    const guess = guessInput.value
+})
+
+guessBtn.addEventListener("click", ()=>{
+    console.log("click")
+    answers.style.visibility = "visible";
+    
+
+})
+
+getRandNum()
 
 
 
