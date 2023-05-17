e38cc83e8149f1a8c409a83e41bd557a92d2b605
bia004 <84ss611@gmail.com>
2023-05-17 15:40:31 -0400

    Add emoji

--- app.js
+++ app.js
@@ -1,23 +1,56 @@
 const guessBtn = document.querySelector('.guess-btn')
+const restartBtn = document.querySelector('.restart-btn')
 const guessInput = document.querySelector('input[type=number]')
 const answers = document.querySelector('.number')
+const userGuess = document.querySelector('.user-guess')
+const correctGuess = document.querySelector('.correct-guess')
+const heading = document.querySelector('.main-heading')
 
-const getRandNum = ()=> {
-    return Math.floor(Math.random()*100) + 1
+
+let randNum;
+let guess;
+
+getRandNum()
+
+function getRandNum(){
+     randNum = Math.floor(Math.random()*10) + 1
 }
 
 guessInput.addEventListener("change", ()=>{
-    const guess = guessInput.value
+   guess = guessInput.value
 })
 
 guessBtn.addEventListener("click", ()=>{
-    console.log("click")
     answers.style.visibility = "visible";
-    
+    userGuess.textContent += guess
+    correctGuess.textContent += randNum
+    guessBtn.style.pointerEvents = "none"
+    guessInput.disabled = true;
+   if(guess == randNum){
+    heading.textContent = "Way to go! 👍"
+   } else {
+    heading.textContent = "Bummer! Try again! 👎"
+   }
 
 })
 
+restartBtn.addEventListener("click", ()=>{
 getRandNum()
+guessBtn.style.pointerEvents = "auto"
+guessInput.disabled = false;
+guessInput.value = ''
+answers.style.visibility = "hidden";
+userGuess.textContent = "You guessed: "
+correctGuess.textContent = "Correct guess: "
+guess = '';
+console.log(randNum)
+heading.textContent = 'Guess the Number ❓'
+})
+
+
+
+
+
 
 
 
